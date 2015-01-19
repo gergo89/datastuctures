5aac5129646aac5a397cdca2be3a7f14216d6158
Gergely Kalapos <gergo@kalapos.net>
2015-01-19 23:18:30 +0100

    Initial Commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Gergely Kalapos on 19/01/15.
+//  Copyright (c) 2015 Gergely Kalapos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
